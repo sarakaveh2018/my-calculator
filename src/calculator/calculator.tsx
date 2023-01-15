811e6ac07935aa5e1e47834f12e831f797934d05
Sara Kaveh <sara kaveh>
2023-01-15 17:18:22 +0330

    initial styling

--- src/calculator/calculator.tsx
+++ src/calculator/calculator.tsx
@@ -1,5 +1,37 @@
+import css from './calculator.module.scss';
+
 export const Calculator = () => {
     return (
-        <div>calculator</div>
+        <div className={css.container}>
+
+            <div className={css.input}>
+                input
+            </div>
+            <div className={css.buttons}>
+                <div className={`${css.button} ${css.clear} ${css.orange}`}>C</div>
+                <div className={`${css.button} ${css.openParenthesis} ${css.lightOrange}`}>(</div>
+                <div className={`${css.button} ${css.closeParenthesis} ${css.lightOrange}`}>)</div>
+                <div className={`${css.button} ${css.multiple} ${css.lightPurple}`}>&#215;</div>
+                <div className={`${css.button} ${css.radical} ${css.lightOrange}`}>@</div>
+                <div className={`${css.button} ${css.percent} ${css.lightOrange}`}>%</div>
+                <div className={`${css.button} ${css.plusMinus} ${css.lightOrange}`}>+</div>
+                <div className={`${css.button} ${css.divide} ${css.lightPurple}`}>&#247;</div>
+                <div className={`${css.button} ${css.seven} ${css.white}`}>7</div>
+                <div className={`${css.button} ${css.eight} ${css.white}`}>8</div>
+                <div className={`${css.button} ${css.nine} ${css.white}`}>9</div>
+                <div className={`${css.button} ${css.minus} ${css.lightPurple}`}>-</div>
+                <div className={`${css.button} ${css.four} ${css.white}`}>4</div>
+                <div className={`${css.button} ${css.five} ${css.white}`}>5</div>
+                <div className={`${css.button} ${css.six} ${css.white}`}>6</div>
+                <div className={`${css.button} ${css.plus} ${css.lightPurple}`}>+</div>
+                <div className={`${css.button} ${css.one} ${css.white}`}>1</div>
+                <div className={`${css.button} ${css.two} ${css.white}`}>2</div>
+                <div className={`${css.button} ${css.three} ${css.white}`}>3</div>
+                <div className={`${css.button} ${css.equal} ${css.purple}`}>=</div>
+                <div className={`${css.button} ${css.dot} ${css.white}`}>.</div>
+                <div className={`${css.button} ${css.zero} ${css.white}`}>0</div>
+                <div className={`${css.button} ${css.back} ${css.lightGray}`}>^</div>
+            </div>
+        </div>
     );
 };
