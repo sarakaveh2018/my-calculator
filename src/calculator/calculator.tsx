02332071c9aed3822178f4fe8a8ef2a5290feaf0
Sara Kaveh <sara kaveh>
2023-01-16 10:46:34 +0330

    created btbValues

--- src/calculator/calculator.tsx
+++ src/calculator/calculator.tsx
@@ -1,5 +1,14 @@
 import css from './calculator.module.scss';
 
+const btnValues = [
+    ["C", "(", ")", "X"],
+    ["R", "%", "PM", "/"],
+    [7, 8, 9, "-"],
+    [4, 5, 6, "+"],
+    [1, 2, 3],
+    [0, ".", "="],
+  ];
+
 export const Calculator = () => {
     return (
         <div className={css.container}>
